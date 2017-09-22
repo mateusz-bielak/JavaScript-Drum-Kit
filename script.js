f02a6372df94987d6161945be8280d88aa6feda7
Mateusz Bielak <mt.bielak@gmail.com>
2017-09-22 16:04:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var keys = [65, 83, 68, 70, 71, 72, 74, 75, 76];
+var letters = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+var sounds = ["clap", "hihat", "kick", "openhat", "boom", "ride", "snare", "tom", "tink"];
+var soundButton = document.querySelectorAll(".sound");
+
+for (var i = 0; i < keys.length; i++) {
+	createButtonElements("sound__key", letters[i], i);
+	createButtonElements("sound__name", sounds[i], i);
+}
+
+function createButtonElements(elementClass, elementText, i) {
+	var element = document.createElement("div");
+	element.className = elementClass;
+	element.textContent = elementText;
+	soundButton[i].appendChild(element);
+}
+
+window.onkeydown = function(event) {
+	var keyValue = event.keyCode;
+	playSound(keyValue);
+}
+
+function playSound(keyValue) {
+	var i = keys.indexOf(keyValue);
+	if (i >= 0) {
+		var audio = new Audio("sounds/" + sounds[i] + ".wav");
+		audio.play();
+	}
+}
