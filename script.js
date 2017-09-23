e046c3582a407812a244bc8c761172dfc98ae39b
Mateusz Bielak <mt.bielak@gmail.com>
2017-09-23 15:37:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 var keys = [65, 83, 68, 70, 71, 72, 74, 75, 76];
 var letters = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 var sounds = ["clap", "hihat", "kick", "openhat", "boom", "ride", "snare", "tom", "tink"];
+var audio = document.querySelectorAll("audio");
 var soundButton = document.querySelectorAll(".sound");
-
-for (var i = 0; i < keys.length; i++) {
-	createButtonElements("sound__key", letters[i], i);
-	createButtonElements("sound__name", sounds[i], i);
-}
 
 function createButtonElements(elementClass, elementText, i) {
 	var element = document.createElement("div");
@@ -15,23 +11,26 @@ function createButtonElements(elementClass, elementText, i) {
 	soundButton[i].appendChild(element);
 }
 
-window.onkeydown = function(event) {
-	var keyValue = event.keyCode;
-	playSound(keyValue);
+function playSound(event) {
+	var key = keys.indexOf(event.keyCode);
+	if (key >= 0) {
+		audio[key].currentTime = 0;
+		audio[key].play();
+		soundButton[key].classList.add("sound--glowing");
+	}
 }
 
-function playSound(keyValue) {
-	var i = keys.indexOf(keyValue);
-	if (i >= 0) {
-		var audio = new Audio("sounds/" + sounds[i] + ".wav");
-		audio.play();
-		soundButton[i].classList.add("sound--glowing");
-	}
+function soundFinished(event) {
+	event.target.classList.remove("sound--glowing");
+}
+
+for (var i = 0; i < keys.length; i++) {
+	createButtonElements("sound__key", letters[i], i);
+	createButtonElements("sound__name", sounds[i], i);
 }
 
 for (var i = 0; i < soundButton.length; i++) {
 	soundButton[i].addEventListener("transitionend", soundFinished);
 }
-function soundFinished(event) {
-	event.target.classList.remove("sound--glowing");
-}
+
+window.addEventListener("keydown", playSound);
